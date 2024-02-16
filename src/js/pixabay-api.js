efff69d3466eec2896aff6aa2060e4e945aea783
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-16 12:06:15 +0100

    k

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -21,3 +21,51 @@ async function getImages() {
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.btnLoadMore.addEventListener('click', loadMore);
+
+const data = await getImages();
+    if (!query) {
+      iziToast.warning({
+        message: 'Sorry, you forgot to enter a search term. Please try again!',
+        position: 'topRight',
+        messageSize: '16px',
+        timeout: 2000,
+      });
+      toggleLoader();
+      return;
+    } else if (parseInt(data.totalHits) > 0) {
+      toggleBtnLoadMore();
+      renderMarkup(data.hits);
+      total = data.totalHits;
+      checkBtnStatus();
+      toggleLoader();
+    } else {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        backgroundColor: 'red',
+        messageColor: 'white',
+        messageSize: '16px',
+        iconColor: 'white',
+        iconUrl: errorIcon,
+        color: 'white',
+        timeout: 2000,
+      });
+      toggleLoader();
+    }
+  } catch (error) {
+    iziToast.error({
+      message: 'Error',
+      position: 'topRight',
+      backgroundColor: 'red',
+      messageColor: 'white',
+      messageSize: '16px',
+      iconColor: 'white',
+      iconUrl: errorIcon,
+      color: 'white',
+      timeout: 2000,
+    });
+    toggleLoader();
+  }
+  event.target.reset();
+}
