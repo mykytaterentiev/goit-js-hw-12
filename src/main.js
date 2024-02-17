35afb8d16341b125f7889fe35393d47fd9428064
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-17 11:08:24 +0100

    l

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import errorIcon from './img/bi_x-octagon.svg';
 import axios from 'axios';
+import { getImages } from './js/pixabay-api';
+import { renderMarkup } from './js/render-functions';
 
 const refs = {
   form: document.querySelector('.form'),
