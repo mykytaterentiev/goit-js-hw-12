eb643428ec4eab9e5eb99ee000f308cb5f9444e1
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-17 11:02:35 +0100

    l

--- src/main.js
+++ src/main.js
@@ -17,26 +17,6 @@ let currentPage = 1;
 let total = 0;
 const PER_PAGE = 15;
 
-async function getImages() {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = import.meta.env.VITE_API_KEY;
-  const url = `${BASE_URL}?key=${API_KEY}`;
-  try {
-    const { data } = await axios.get(url, {
-      params: {
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        q: query,
-        per_page: PER_PAGE,
-        page: currentPage,
-      },
-    });
-    return data;
-  } catch (error) {
-    console.error("Сталася помилка при отриманні зображень:", error.message);
-  }
-}
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.btnLoadMore.addEventListener('click', loadMore);
