8d586b4a1e8980c5d3f1c019c6fc6bd2b8c4cc8e
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-17 12:05:51 +0100

    д

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,3 +1,22 @@
+function galleryTemplate({
+  largeImageURL,
+  webformatURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<a class='gallery-link' href='${largeImageURL}'><img class='gallery-image' src='${webformatURL}' alt='${tags}'/>
+  <div class='gallery-review'>
+  <div class='gallery-review-item'><b>Likes</b> <span>${likes}</span></div>
+  <div class='gallery-review-item'><b>Views</b> <span>${views}</span></div>
+  <div class='gallery-review-item'><b>Comments</b> <span>${comments}</span></div>
+  <div class='gallery-review-item'><b>Downloads</b> <span>${downloads}</span></div>
+  </div></a>
+    `;
+}
+
 export function renderMarkup(images) {
   const markup = images.map(galleryTemplate).join('');
   refs.gallery.insertAdjacentHTML('beforeend', markup);
