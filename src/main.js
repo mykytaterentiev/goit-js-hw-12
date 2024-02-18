230e7f118262bd8406631fe6f1a3d05d5872b705
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-18 09:53:02 +0100

    l

--- src/main.js
+++ src/main.js
@@ -85,24 +85,6 @@ async function onFormSubmit(event) {
   event.target.reset();
 }
 
-function galleryTemplate({
-  largeImageURL,
-  webformatURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) {
-  return `<a class='gallery-link' href='${largeImageURL}'><img class='gallery-image' src='${webformatURL}' alt='${tags}'/>
-  <div class='gallery-review'>
-  <div class='gallery-review-item'><b>Likes</b> <span>${likes}</span></div>
-  <div class='gallery-review-item'><b>Views</b> <span>${views}</span></div>
-  <div class='gallery-review-item'><b>Comments</b> <span>${comments}</span></div>
-  <div class='gallery-review-item'><b>Downloads</b> <span>${downloads}</span></div>
-  </div></a>
-    `;
-}
 
 let gallery = new SimpleLightbox('.gallery a', {
   showCounter: false,
