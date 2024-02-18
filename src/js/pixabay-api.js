e094078bedd960daf51fc4ff89d911bafab08f86
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-18 15:07:33 +0100

    15-07

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,8 @@
 
 import axios from 'axios';
 
+let query = '';
+
 export async function getImages() {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '42272856-fbb9ecaa9aa7f62044da3b204';
