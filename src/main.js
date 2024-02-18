050f93d8badf4e895ea120fd4b06cf6bc7404a07
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-18 15:03:14 +0100

    15-03

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import errorIcon from './img/bi_x-octagon.svg';
 import axios from 'axios';
+import { getImages } from './js/pixabay-api';
 
 const refs = {
   form: document.querySelector('.form'),
@@ -16,27 +17,6 @@ let query = '';
 let currentPage = 1;
 let total = 0;
 const PER_PAGE = 15;
-
-async function getImages() {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '42272856-fbb9ecaa9aa7f62044da3b204';
-  const url = `${BASE_URL}?key=${API_KEY}`;
-  try {
-    const { data } = await axios.get(url, {
-      params: {
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        q: query,
-        per_page: PER_PAGE,
-        page: currentPage,
-      },
-    });
-    return data;
-  } catch (error) {
-    console.error("Сталася помилка при отриманні зображень:", error.message);
-  }
-}
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.btnLoadMore.addEventListener('click', loadMore);
