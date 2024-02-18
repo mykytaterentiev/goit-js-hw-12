d307d3186e708506d22e2cb78b31ac1eb828672e
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-18 09:56:17 +0100

    ;

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ const PER_PAGE = 15;
 
 async function getImages() {
   const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = import.meta.env.VITE_API_KEY;
+  const API_KEY = '42272856-fbb9ecaa9aa7f62044da3b204';
   const url = `${BASE_URL}?key=${API_KEY}`;
   try {
     const { data } = await axios.get(url, {
